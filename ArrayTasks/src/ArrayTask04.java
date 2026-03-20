af098ebea86e4aaa5040d80e76a763aa64ab5d73
Dilyar444 <gapparov412@gmail.com>
2026-03-20 23:04:25 +0600

    Added ArrayList tasks

---
+++ ArrayTasks/src/ArrayTask04.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayTask04 {
+    public static void main(String[] args) {
+        ArrayList<String> contactsInPhone = new ArrayList<String>(List.of("Andrew", "Ann"));
+        ArrayList<String> contactsInSim = new ArrayList<String>(List.of("Alya", "Ann"));
+        System.out.println(contactsInSim);
+        System.out.println(contactsInPhone);
+        for (String name: contactsInPhone) {
+            if (!contactsInSim.contains(name)) {
+                contactsInSim.add(name);
+            }
+        }
+        System.out.println(contactsInSim);
+    }
+}
